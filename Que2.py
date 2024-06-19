8f572305f1a82d756a64bb95ef6dcc4eb20c0329
Arshima-Sheikh <168852376+Arshima-Sheikh@users.noreply.github.com>
2024-06-19 13:54:31 +0530

    Create Que2.py

---
+++ Que2.py
@@ -0,0 +1,13 @@
+
+def check_even_odd():
+    
+    number = int(input("Enter a number: "))
+
+   
+    if number % 2 == 0:
+        print("even number")
+    else:
+        print("odd number")
+
+
+check_even_odd()
