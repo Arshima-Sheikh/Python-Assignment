a5009d8787913d88fe96e24d2f9171b91246a7d9
Arshima-Sheikh <168852376+Arshima-Sheikh@users.noreply.github.com>
2024-06-19 20:16:32 +0530

    Create que27.py

---
+++ que27.py
@@ -0,0 +1,9 @@
+
+def string_to_char_list(string):
+    return list(string)
+
+
+string = input("Enter the string: ")
+
+char_list = string_to_char_list(string)
+print(f"The list of characters is: {char_list}")
