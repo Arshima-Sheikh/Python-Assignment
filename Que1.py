7364417836cbc8026893ff9b3ff0cc6e5e8b1366
Arshima-Sheikh <168852376+Arshima-Sheikh@users.noreply.github.com>
2024-06-19 13:53:21 +0530

    Create Que1.py

---
+++ Que1.py
@@ -0,0 +1,13 @@
+def sum():
+   
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+   
+    total = num1 + num2
+
+    
+    print(total)
+
+
+sum()
