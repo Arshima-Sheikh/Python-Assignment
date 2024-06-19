1f60d7c26efe4fd17097dae8d489372e50fde388
Arshima-Sheikh <168852376+Arshima-Sheikh@users.noreply.github.com>
2024-06-19 13:57:05 +0530

    Create Ques3.py

---
+++ Ques3.py
@@ -0,0 +1,14 @@
+
+def calculate_factorial():
+    import math
+    
+   
+    number = int(input("Enter a number: "))
+  
+    factorial = math.factorial(number)
+    
+   
+    print("The factorial of {} is {}".format(number, factorial))
+
+
+calculate_factorial()
