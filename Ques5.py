e696e436ad3fff1245fef6d7b5bc7a6e1a989bfb
Arshima-Sheikh <168852376+Arshima-Sheikh@users.noreply.github.com>
2024-06-19 13:58:55 +0530

    Create Ques5.py

---
+++ Ques5.py
@@ -0,0 +1,13 @@
+
+def write_to_file():
+    
+    user_input = input("Enter a string to write to the file: ")
+    
+   
+    with open("output.txt", "w") as file:
+        file.write(user_input)
+    
+    print("The input has been written to output.txt")
+
+
+write_to_file()
